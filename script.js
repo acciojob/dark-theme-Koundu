e7311d545f8cebf9ec08dabe883d544a86c77fec
Koundinya G <121346044+Koundu@users.noreply.github.com>
2024-01-18 23:47:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
-
+document.getElementById("swap").addEventListener('click',()=>{
+	swapTheme();
+})
+function swapTheme(){
+	if(document.getElementById("app").style.backgroundColor == "black"){
+		document.getElementById("app").style.backgroundColor = "white"
+	}else if(document.getElementById("app").style.backgroundColor = "white"){
+		document.getElementById("app").style.backgroundColor = "black"
+	}	
+}
